0128b0152c43126671c17ac135289937c1b17691
Kipman <felippekipman@cjr.org.br>
2017-01-20 00:15:04 -0200

    -Added create action to refunds controller -Defined all other RESTful actions in refunds controller (did not implement) -Added view for created refunds

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -1,8 +1,32 @@
 class RefundsController < ApplicationController
+  def index
+  end
+
+  def show
+    @article = Refund.find(params[:id])
+  end
+
   def new
   end
 
-  def create
-    render plain: params[:refund].inspect
+  def edit
   end
+
+  def create
+    @refund = Refund.new(refund_params)
+
+    @refund.save
+    redirect_to @refund
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def refund_params
+      params.require(:refund).permit(:value, :description)
+    end
 end
